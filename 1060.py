c4faa1a0d25eb155ef21ca4694b1dc2ed5190fa8
VitorBriosp <167243828+VitorBriosp@users.noreply.github.com>
2024-04-16 23:48:50 -0400

    Create 1060.py

---
+++ 1060.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+contador = 0
+a1 = float(input())
+a2 = float(input())
+a3 = float(input())
+a4 = float(input())
+a5 = float(input())
+a6 = float(input())
+if a1 > 0:
+    contador+=1
+if a2 > 0:
+    contador+=1
+if a3 > 0:
+    contador+=1
+if a4 > 0:
+    contador+=1
+if a5 > 0:
+    contador+=1
+if a6 > 0:
+    contador+=1
+print('{} valores positivos'.format(contador))
