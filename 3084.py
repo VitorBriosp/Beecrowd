19c2ffa1438f6dc485ffad40ec8dcae5a40949b8
Vitor Barbeta <167243828+VitorBriosp@users.noreply.github.com>
2024-04-20 11:34:45 -0400

    Update 3084.py

--- 3084.py
+++ 3084.py
@@ -6,10 +6,6 @@ while True:
         y = int(b)
         cal = x // 30
         cal2 = y // 6
-        if cal < 10:
-            cal = '0{}'.format(x // 30)
-        if cal2 < 10:
-            cal2 = '0{}'.format(y // 6)
-        print('{}:{}'.format(cal, cal2))
+        print('{:02}:{:02}'.format(cal, cal2))
     except EOFError:
         break
