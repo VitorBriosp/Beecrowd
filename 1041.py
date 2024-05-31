c52451bcff1b0e96b9f8125bd5674d18408ba96b
Vitor Barbeta <167243828+VitorBriosp@users.noreply.github.com>
2024-05-30 21:26:54 -0400

    Update 1041.py

--- 1041.py
+++ 1041.py
@@ -1,3 +1,8 @@
+'''
+Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano.
+A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+'''
+
 # -*- coding: utf-8 -*-
 a, b = input().split()
 x = float(a)
