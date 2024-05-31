abf3d4687187565dbb893a74889f38f28862c85c
Vitor Barbeta <167243828+VitorBriosp@users.noreply.github.com>
2024-05-30 21:29:07 -0400

    Update 1044.py

--- 1044.py
+++ 1044.py
@@ -1,3 +1,9 @@
+'''
+Leia 2 valores inteiros (A e B). Após, o 
+programa deve mostrar uma mensagem 
+"Sao Multiplos" ou "Nao sao Multiplos",
+indicando se os valores lidos são múltiplos entre si.
+'''
 # -*- coding: utf-8 -*-
 a, b = input().split()
 x = float(a)
