8c66838c84e4c92e5f9a4a004a99efc18be8eae3
Vitor Barbeta <167243828+VitorBriosp@users.noreply.github.com>
2024-05-30 21:26:01 -0400

    Create 1181.py

---
+++ 1181.py
@@ -0,0 +1,24 @@
+'''
+Neste problema você deve ler um número, indicando uma linha da 
+matriz na qual uma operação deve ser realizada, um caractere maiúsculo,
+indicando a operação que será realizada, e todos os elementos de uma matriz M[12][12].
+Em seguida, calcule e mostre a soma ou a média dos elementos que estão na área verde da matriz,
+conforme for o caso. A imagem abaixo ilustra o caso da entrada do valor 2 para a linha da matriz, 
+demonstrando os elementos que deverão ser considerados na operação.
+'''
+
+# -*- coding: utf-8 -*-
+n = int(input())
+s = input()
+soma = 0
+for i in range(12):
+    l=[]
+    for j in range(12):
+        a = float(input())
+        l.append(a)
+        if i == n:
+            soma+=l[j]
+if s == 'S':
+    print('{:.1f}'.format(soma))
+elif s == 'M':
+    print('{:.1f}'.format(soma / 12))
